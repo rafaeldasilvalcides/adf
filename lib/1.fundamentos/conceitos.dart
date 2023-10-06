c2c006271db6b2dcdfa8d6c20fcd70ce19124a80
Rafael da Silva Alcides <rafaeldasilvalcides@gmail.com>
2023-10-05 22:11:24 -0300

    first commit : Conceitos Importantes

---
+++ lib/1.fundamentos/conceitos.dart
@@ -0,0 +1,13 @@
+//?Existe Função de nivel Superior
+void main() {
+
+  //? O dart é uma Linguagem Orientada a objeto
+  //? Tudo no dart é um Objetos
+  //? Objetos são classes
+  print(nome);
+  
+}
+
+String? nome;
+//? Existe no dart Variaveis de nivel Superior
+//? Variaveis de nivel Superior são variaveis soltas dentro do Arquivo Dart
