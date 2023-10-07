c976459321d589a64e959d8848c66f189520299f
Rafael da Silva Alcides <rafaeldasilvalcides@gmail.com>
2023-10-06 22:37:00 -0300

    second commit : Variaveis e tipos

---
+++ lib/2.variaveis/variaveis_e_tipos.dart
@@ -0,0 +1,15 @@
+//? Variaveis são Pequenos espaços na memoria do computador
+//? 
+
+//? Função main é uma função de start
+void main() {
+//! Tipos de Variaveis
+//? Numeros
+int idade = 23; //* Variavel do Tipo inteiro
+double preco = 19.9; //* Variavel do Tipo Decimal
+num qualquerNumero = 123; //* Variavel do tipo num pode receber inteiro ou decimal 
+String nome = 'Rafael'; //* Variavel do tipo String
+var variavel = 23; //* Variavel do tipo var recebe o valor que se inicia
+Object variavel1 = 12; //* Todo mundo é filho de object Então pode se inferir qualquer valor
+dynamic variavel2 = 12; //* Variavel do tipo dynamic recebe qualquer tipo tbm cuidado
+}
